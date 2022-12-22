cbff39a5ef5e9fea8478ebf1dd3ac8d3a116d408
alena <gmaidenmueller@gmail.com>
2022-12-22 12:25:54 -0600

    make add fields match edit

--- src/Controller/Admin/ClinicCrudController.php
+++ src/Controller/Admin/ClinicCrudController.php
@@ -28,6 +28,9 @@ class ClinicCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         $name = TextField::new('name');
+        $description = TextareaField::new('description');
+        $latitude = NumberField::new('latitude')->setColumns(5);
+        $longitude = NumberField::new('longitude')->setColumns(5);
         $dataSource = TextField::new('dataSource');
         $updatedOn = DateTimeField::new('updatedOn')->setFormTypeOptions([
             'html5' => true,
@@ -41,14 +44,19 @@ class ClinicCrudController extends AbstractCrudController
         if (Crud::PAGE_EDIT === $pageName) {
             yield FormField::addPanel('Details');
             yield $name;
-            yield TextareaField::new('description');
-            yield NumberField::new('latitude')->setColumns(5);
-            yield NumberField::new('longitude')->setColumns(5);
+            yield $description;
+            yield $latitude;
+            yield $longitude;
 
             yield FormField::addPanel('Metadata');
             yield $dataSource->setFormTypeOption('disabled', true);
             yield $updatedOn->setFormTypeOption('disabled', true);
             yield $importedOn->setFormTypeOption('disabled', true);
+        } elseif (Crud::PAGE_NEW === $pageName) {
+            yield $name;
+            yield $description;
+            yield $latitude;
+            yield $longitude;
         } else {
             yield $name;
             yield $dataSource;
