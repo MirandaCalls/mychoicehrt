00bbf15ce9f2815093956595dc3686577bf7dfdb
alena <gmaidenmueller@gmail.com>
2022-12-31 18:01:58 -0600

    check for duplicates whenever a clinic is added or updated

--- src/EntityListener/ClinicEntityListener.php
+++ src/EntityListener/ClinicEntityListener.php
@@ -3,19 +3,35 @@
 namespace App\EntityListener;
 
 use App\Entity\Clinic;
+use App\Message\FindDuplicatesMessage;
 use App\Repository\DuplicateLinkRepository;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class ClinicEntityListener
 {
+    private MessageBusInterface $bus;
     private DuplicateLinkRepository $duplicates;
 
-    public function __construct(DuplicateLinkRepository $duplicates)
-    {
+    public function __construct(
+        MessageBusInterface $bus,
+        DuplicateLinkRepository $duplicates,
+    ) {
+        $this->bus = $bus;
         $this->duplicates = $duplicates;
     }
 
-    public function preRemove(Clinic $clinic, LifecycleEventArgs $event)
+    public function postPersist(Clinic $clinic, LifecycleEventArgs $args): void
+    {
+        $this->bus->dispatch(new FindDuplicatesMessage($clinic->getId()));
+    }
+
+    public function postUpdate(Clinic $clinic, LifecycleEventArgs $args): void
+    {
+        $this->bus->dispatch(new FindDuplicatesMessage($clinic->getId()));
+    }
+
+    public function preRemove(Clinic $clinic, LifecycleEventArgs $args): void
     {
         $this->duplicates->deleteForClinicId($clinic->getId());
     }
