33cf448ca3844b3e8d7e99606a056e8c71757bd2
alena <gmaidenmueller@gmail.com>
2023-01-06 14:19:02 -0600

    overlay the search radius over the clinic map markers

--- assets/search.ts
+++ assets/search.ts
@@ -1,8 +1,10 @@
 import * as $ from 'jquery';
 import * as leaflet from 'leaflet';
-import {Map} from "leaflet";
+import {LatLngExpression, Map} from "leaflet";
 import './styles/pages/search.scss';
 import initSearchFormHandlers from './searchForm';
+
+const METERS_IN_MILE = 1609.344;
 
 $(() => {
    initSearchFormHandlers();
@@ -10,6 +12,7 @@ $(() => {
    const previewMapDiv = $('#previewMap');
    const originLatitude = previewMapDiv.data('origin-latitude');
    const originLongitude = previewMapDiv.data('origin-longitude');
+   const searchRadius = previewMapDiv.data('search-radius');
 
    const map = leaflet.map('previewMap');
    map.setView([originLatitude, originLongitude], 13);
@@ -18,6 +21,7 @@ $(() => {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
    }).addTo(map);
 
+   plotSearchRadius(map, [originLatitude, originLongitude], searchRadius);
    plotClinics(map);
 });
 
@@ -34,4 +38,12 @@ function plotClinics(map: Map) {
 
       leaflet.marker([latitude, longitude], {icon: icon}).addTo(map);
    });
+}
+
+function plotSearchRadius(map: Map, coordinate: LatLngExpression, searchRadius: number) {
+   const circle = leaflet.circle(coordinate, {
+      radius: searchRadius * METERS_IN_MILE,
+   });
+   circle.addTo(map);
+   map.fitBounds(circle.getBounds());
 }
