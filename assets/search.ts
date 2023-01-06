e682780274bdb57db84fc1af7f89565ffa5323a2
alena <gmaidenmueller@gmail.com>
2023-01-06 10:58:44 -0600

    display leaflet map in search results

--- assets/search.ts
+++ assets/search.ts
@@ -1,7 +1,19 @@
 import * as $ from 'jquery';
+import * as leaflet from 'leaflet';
 import './styles/pages/search.scss';
 import initSearchFormHandlers from './searchForm';
 
 $(() => {
    initSearchFormHandlers();
+
+   const previewMapDiv = $('#previewMap');
+   const originLatitude = previewMapDiv.data('origin-lat');
+   const originLongitude = previewMapDiv.data('origin-long');
+
+   const map = leaflet.map('previewMap');
+   map.setView([originLatitude, originLongitude], 13);
+   leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+   }).addTo(map);
 });
