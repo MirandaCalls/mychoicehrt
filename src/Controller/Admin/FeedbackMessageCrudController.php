728b24720b299a27afcd019eb6c1ac010b7ac81e
alena <gmaidenmueller@gmail.com>
2023-01-18 13:21:20 -0600

    display clinics linked to feedback in the admin dashboard

--- src/Controller/Admin/FeedbackMessageCrudController.php
+++ src/Controller/Admin/FeedbackMessageCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
@@ -41,6 +42,7 @@ class FeedbackMessageCrudController extends AbstractCrudController
         yield ChoiceField::new('feedbackType')
             ->setChoices(FeedbackMessage::FEEDBACK_TYPES)
         ;
+        yield AssociationField::new('clinic');
         yield TextareaField::new('messageText');
         yield DateTimeField::new('submittedOn')->setFormTypeOption('disabled', true);
     }
