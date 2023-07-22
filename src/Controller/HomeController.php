bcbbefe27ff0e67c655263d450c3214901dc6f52
alena <gmaidenmueller@gmail.com>
2023-07-21 21:52:54 -0500

    remove test endpoint

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,11 +24,4 @@ class HomeController extends AbstractController
             'searchForm' => $searchForm,
         ]);
     }
-
-    #[Route('/error', name: 'app_error_test')]
-    public function errorTest(): Response
-    {
-        throw new \Exception('fish!');
-        return new Response('test', Response::HTTP_OK);        
-    }
 }
